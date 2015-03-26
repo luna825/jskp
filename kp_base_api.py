0950938781253280c43db066109f37d1cf5ba7da
luna825 <luna825@qq.com>
2015-03-26 22:17:44 +0800

    request_token完成
    第一次request_token测试

---
+++ kp_base_api.py
@@ -0,0 +1,63 @@
+# coding=utf-8
+import time,math,urllib,hmac,hashlib,binascii,json,re,urllib2,cookielib
+
+'''一些基础的接口实现'''
+count = 0
+def get_oauth_nonce():
+	'''
+	生成oauth_nonce
+	'''
+	global count
+	count += 1
+	return str(time.time() + 1).replace(".","")
+
+def get_timestamp():
+	'''
+	生成oauth_timestamp
+	'''
+	return int(math.ceil(time.time()))
+
+def get_base_string(base_url,param_dic,method="GET"):
+	'''
+	生成signature需要的base_string
+	'''
+	l = [urllib.quote_plus(k)+"="+urllib.quote_plus(v) for k,v in param_dic.iteritems()]
+	l.sort()
+	return method + '&' + urllib.quote_plus(base_url)+'&'+urllib.quote_plus('&'.join(l))
+def get_signature(base_string,consumer_secret_and_oauth_token):
+	binary_sig = hmac.new(consumer_secret_and_oauth_token,base_string,hashlib.sha1)
+	return binascii.b2a_base64(binary_sig.digest())[:-1]
+
+def build_base_param(consumer_key):
+	'''
+	生成一些基础的参数。每次请求这需要用这些参数
+	'''
+	dic={}
+	dic["oauth_nonce"] = get_oauth_nonce()
+	dic["oauth_timestamp"]= str(get_timestamp())
+	dic["oauth_consumer_key"]=consumer_key
+	dic["oauth_signature_method"]="HMAC-SHA1"
+	dic["oauth_version"]="1.0"
+	return dic
+
+def build_request_url(consumer_key,consumer_secret,base_url,oauth_token="",
+	oauth_token_secret="",extra_params={},method="GET"):
+	dic = build_base_param(consumer_key)
+	if len(oauth_token)!=0:
+		dic["oauth_token"]=oauth_token
+	dic.update(extra_params)
+	signature = get_signature(get_base_string(base_url,dic,method),
+		consumer_secret+"&"+oauth_token_secret)
+	dic["oauth_sinature"]=signature
+	url = base_url + "?" + urllib.urlencode(dic)
+	return url
+
+def request_token(consumer_key,consumer_secret):
+	url = build_request_url(consumer_key,consumer_secret,
+		"https://openapi.kuaipan.cn/open/requestToken")
+	print url
+	json_text = urllib.urlopen(url).read()
+	j = json.loads(json_text)
+	oauth_token = j["oauth_token"].encode("ascii")
+	oauth_token_secret = j["oauth_token_secret"].encode("ascii")
+	return (oauth_token,oauth_token_secret)
